cc7ff78f19de2c56e54356f7280091e54cfd7746
NazmulHasanShefat <119728546+NazmulHasanShefat@users.noreply.github.com>
2025-06-03 18:32:58 +0600

    last commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-   var swiper = new Swiper(".mySwiper", {
+// script for sweper slider
+
+var swiper = new Swiper(".mySwiper", {
       pagination: {
         el: ".swiper-pagination",
       },
-    });
+});
 
+
+
+
+// fetch product from api
 async function fetchProducts() {
   try{
     let res = await fetch("https://dummyjson.com/products");
@@ -15,12 +21,13 @@ async function fetchProducts() {
     return error
   }
 }
+// include product in html use api data
 fetchProducts()
    .then(product_data =>{
     console.log(product_data);
       let all_product_data = product_data.products;
       console.log(all_product_data);
-      
+      // adding product
       function addProduct_from_api(){
         let product_list_container = document.querySelector(".products-list-container");
         product_list_container.innerHTML = `
@@ -34,23 +41,23 @@ fetchProducts()
           <div class="pt bg-success text-white">Sale</div>
         </div>
         <div class="product-image d-flex justify-content-center position-relative" style="width: 100%;">
-          <img src="${productItem.images}" height="200px" width="190px" alt="">
+          <img src="${productItem.images}" height="200px" width="190px" alt="" style="cursor: pointer;">
           <div class="shot_cut_icons position-absolute justify-content-center gap-3"
             style="width: 100%; height: max-content;">
-            <div class="sc-icons"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="18" height="18"
+            <div class="sc-icons" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Quick view"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="14" height="14"
                 fill="currentColor" class="bi bi-eye">
                 <path
                   d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z">
                 </path>
                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"></path>
               </svg></div>
-            <div class="sc-icons"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="18" height="18"
+            <div class="sc-icons" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Add to Wishlist"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="14" height="14"
                 fill="currentColor" class="bi bi-heart">
                 <path
                   d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15">
                 </path>
               </svg></div>
-            <div class="sc-icons"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="18" height="18"
+            <div class="sc-icons" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Compare"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="14" height="14"
                 fill="currentColor" class="bi bi-arrow-left-right">
                 <path fill-rule="evenodd"
                   d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5">
@@ -59,7 +66,7 @@ fetchProducts()
           </div>
         </div>
         <div class="s-icons product-catagory">
-          <p class="mb-1">Snack & Munchies</p>
+          <p class="mb-1">${productItem.tags.join(" & ")}</p>
         </div>
         <div class="product-name">
           <h4 class="mb-1 mt-2">${productItem.title}</h4>
@@ -97,13 +104,13 @@ fetchProducts()
               </path>
             </svg>
           </div>
-          <div class="point" id="R_point${productItem.id}">${Math.floor(productItem.rating)}</div>
+          <div class="point" id="R_point${productItem.id}"><span>${Math.floor(productItem.rating)}</span></div>
         </div>
          <div class="stok-status">
           <p>${productItem.availabilityStatus}</p>
         </div>
         <div class="p-bottom-section d-flex align-items-center justify-content-between mt-3">
-          <div class="price"> <strong>$${productItem.price}</strong> <span class="old-price"> $25.5</span></div>
+        <div class="price"> <strong>$${productItem.price}</strong> <span class="old-price"> $25.5</span></div>
           <div class="add-tocart-button">
             <button style="border-radius: 4px;" class="px-3 py-1 text-white">+Add</button>
           </div>
@@ -118,45 +125,45 @@ fetchProducts()
       addProduct_from_api();
 
 
+      // script fot bootstrap tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+      // product rating functionality adding
       let productStartRating = document.querySelectorAll(".point");
       let select_product_star = document.querySelector(".stars");
-   
-      // console.log(select_product_star.children[4]);
-      // let x = productStartRating.innerText;
-      // let y = document.getElementById();
-      // console.log(y)
+      
+      // product rating functionality adding
       for(let pt = 0; pt<productStartRating.length; pt++){
-         console.log(productStartRating[pt].id);
+        //  console.log(productStartRating[pt].id);
          let pd_rating_point_id = document.getElementById(productStartRating[pt].id);
 
          let rt_point_text = pd_rating_point_id.innerText;
-         
-         let four_star_rating_icons_parent = pd_rating_point_id.parentElement.firstElementChild;
+         let star_rating_icons_parent = pd_rating_point_id.parentElement.firstElementChild;
+
          
          if(rt_point_text == 4){
             console.log("4 star is found");
-            // console.log(pd_rating_point_id.parentElement.firstElementChild);
-              four_star_rating_icons_parent.children[0].style.fill = "#ffa534";
-              four_star_rating_icons_parent.children[1].style.fill = "#ffa534";
-              four_star_rating_icons_parent.children[2].style.fill = "#ffa534";
-              four_star_rating_icons_parent.children[3].style.fill = "#ffa534";
+            console.log(star_rating_icons_parent);
+              star_rating_icons_parent.children[0].style.fill = "#ffa534";
+              star_rating_icons_parent.children[1].style.fill = "#ffa534";
+              star_rating_icons_parent.children[2].style.fill = "#ffa534";
+              star_rating_icons_parent.children[3].style.fill = "#ffa534";
          }else if(rt_point_text == 3){
-          
+              star_rating_icons_parent.children[0].style.fill = "#ffa534";
+              star_rating_icons_parent.children[1].style.fill = "#ffa534";
+              star_rating_icons_parent.children[2].style.fill = "#ffa534";
+         }else if(rt_point_text == 2){
+              star_rating_icons_parent.children[0].style.fill = "#ffa534";
+              star_rating_icons_parent.children[1].style.fill = "#ffa534";
+           
+         }else if(rt_point_text == 1){
+              star_rating_icons_parent.children[0].style.fill = "#ffa534";
          }
          else{console.log("nothing")};
-
-
-         
-         
-         console.log(rt_point_text);
+          console.log(rt_point_text);
         }
 
-      
-
-
-
-      
 
    })
 
